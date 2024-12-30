0141e0b8656661143251537795f7cc11afaea8c3
hari <sbvh1437@gmail.com>
2024-12-30 15:50:25 +0530

    added a

--- myapp/app.js
+++ myapp/app.js
@@ -1,5 +1,9 @@
 const express = require('express');
+<<<<<<< HEAD
 monst app = express();
+=======
+const app = express();
+>>>>>>> parent of 47c4269 (changes to my dev file)
 const port = 3000;
 
 app.get('/', (req, res) => {
